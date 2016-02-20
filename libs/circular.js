5d2391969ebc65a49c729c0817e0e4cdfbbf5f06
dimatim <dim882@gmail.com>
2016-02-20 18:37:01 +0200

    added dismiss loading functionality to circular.js

--- libs/circular.js
+++ libs/circular.js
@@ -2,10 +2,19 @@
  * Created by Dima on 07-Feb-16.
  */
 
+var stopArray = [];
+
+function stopSpinner(canvasId) {
+    stopArray.push(canvasId);
+}
+
 function startSpinner(data) {
     if (data['canvas'] == null || data['colors'] == null)
         return;
     data['adjustPosition'] = 0;
+    data['radianIncrement'] = Math.PI / 180;
+    if (data['duration'] == null)
+        data['duration'] = 2000;
     if (data['spacing'] == null)
         data['spacing'] = 0;
     if (data['repeat'] == null)
@@ -22,24 +31,47 @@ function startSpinner(data) {
     animate(data);
 }
 
-//TODO add stop animation
-/*function stopSpinner() {
- }*/
-
 function animate(data) {
     var reverse = data.reverse;
     var dir = reverse ? -1 : 1;
     $({progress: reverse ? 1 : 0}).animate({progress: reverse ? 0 : 1}, {
-        duration: 3000,
+        duration: data.duration,
         easing: "linear",
         step: function (now) {
-            data.adjustPosition += Math.PI / 90 * dir; // speed
+            data.adjustPosition += data.radianIncrement * dir; // speed
+            data.adjustPosition %= 2 * Math.PI;
             create(now, data);
         },
         complete: function () {
-            data.adjustPosition += data.maxRadians * dir;
+            if ($.inArray(data.canvas.attr('id'), stopArray) > -1) {
+                stopArray.splice(stopArray.indexOf(data.canvas.attr('id')), 1);
+                endSpin(data);
+            } else {
+                data.adjustPosition += data.maxRadians * dir;
+                data.adjustPosition %= 2 * Math.PI;
+                animate(data);
+            }
+        }
+    });
+}
+
+function endSpin(data) {
+    var reverse = data.reverse;
+    var dir = reverse ? -1 : 1;
+    if (!data.reverse) {
+        data.adjustPosition += data.maxRadians * dir;
+        data.adjustPosition %= 2 * Math.PI;
+    }
+    data.maxRadians = 2 * Math.PI;
+    $({progress: reverse ? 1 : 0}).animate({progress: .5}, {
+        duration: duration / 3,
+        easing: "linear",
+        step: function (now) {
+            data.adjustPosition += data.radianIncrement * dir; // speed
             data.adjustPosition %= 2 * Math.PI;
-            animate(data)
+            var context = data.canvas[0].getContext('2d');
+            context.globalAlpha = 1 - (data.reverse ? Math.abs(now - 1) : now) * 2;
+            create(now, data);
         }
     });
 }
@@ -47,7 +79,7 @@ function animate(data) {
 function create(now, data) {
     var context = data.canvas[0].getContext('2d');
     context.clearRect(0, 0, data.canvas.width(), data.canvas.height());
-    var maxNumIterations = Math.min((data.maxRadius - 2) / (data.lineWidth + data.spacing), data.repeat * data.colors.length);
+    var maxNumIterations = Math.ceil(Math.min((data.maxRadius - 2) / (data.lineWidth + data.spacing), data.repeat * data.colors.length));
     for (var i = 0; i < maxNumIterations; ++i) {
         drawCircle(data, i, now);
     }
@@ -69,3 +101,21 @@ function drawCircle(data, iteration, fraction) {
     context.arc(size[0], size[1], radius, start, end);
     context.stroke();
 }
+
+/*function drawCircleEnd(data, iteration, fraction) {
+ var fillFraction = data.reverse ? Math.abs(fraction - 1) : fraction;
+ var context = data.canvas[0].getContext('2d');
+ var size = [data.canvas.width() / 2, data.canvas.height() / 2];
+ var index = iteration % data.colors.length;
+ var radius = iteration == 0 ? Math.max(data.maxRadius / 2, (data.maxRadius - data.maxRadius * fillFraction * 2)) : (data.maxRadius - iteration * (data.lineWidth + data.spacing));
+ var color = data.colors[index].color;
+ var offset = data.colors[index].offset;
+ context.beginPath();
+ context.lineWidth = iteration == 0 ? Math.min(data.maxRadius, (data.lineWidth + (data.maxRadius - data.lineWidth) * fillFraction * 4)) : data.lineWidth;
+ context.strokeStyle = color;
+ var reverse = fraction > .5;
+ var start = offset + data.adjustPosition + (reverse ? (fraction - .5) * 2 * data.maxRadians : 0);
+ var end = data.minRadians + offset + data.adjustPosition + (reverse ? data.maxRadians : fraction * 2 * data.maxRadians);
+ context.arc(size[0], size[1], radius, start, end);
+ context.stroke();
+ }*/
